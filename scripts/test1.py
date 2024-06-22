6e773f780525ae7702ccb6036ee4322110e0e3f9
backtrac3r <medvedosandr@gmail.com>
2024-06-22 17:26:41 +0300

    .

--- scripts/test1.py
+++ scripts/test1.py
@@ -2,12 +2,7 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 headers = {
-    # 'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Referer': 'http://www.wikipedia.org/',
-    'Connection': 'keep-alive',
 }
 
 params = {
