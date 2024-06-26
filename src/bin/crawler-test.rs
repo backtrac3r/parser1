268938ed895f0524937622b9636c4f525cf40bd1
rey256 <medvedosandr@gmail.com>
2024-06-26 17:19:16 +0300

    .

---
+++ src/bin/crawler-test.rs
@@ -0,0 +1,13 @@
+use spider::tokio;
+use spider::website::Website;
+
+#[tokio::main]
+async fn main() {
+    let url = "https://choosealicense.com";
+    let mut website = Website::new(url);
+    website.crawl().await;
+
+    for link in website.get_links() {
+        println!("- {:?}", link.as_ref());
+    }
+}
--- src/bin/crawler.rs
+++ src/bin/crawler.rs
@@ -12,10 +12,12 @@ async fn main() {
     let url = matches.get_one::<String>("url").unwrap();
 
     let config = Configuration::new()
-        .with_user_agent(Some("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"))
-        // .with_depth(30)
-        .with_redirect_limit(2)
-        .with_respect_robots_txt(true)
+        // .with_user_agent(Some("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"))
+        // .with_chrome_intercept(cfg!(feature = "chrome_intercept"), true)
+        // .with_depth(0)
+        // .with_redirect_limit(10000)
+        // .with_caching(true)
+        // .with_respect_robots_txt(true)
         .build();
 
     let mut website = Website::new(url)
