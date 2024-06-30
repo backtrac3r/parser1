9e29841d566bb69fc64e94fb5243d52cbe0acf2a
rey256 <medvedosandr@gmail.com>
2024-07-01 00:02:32 +0300

    .

--- src/bin/crawler.rs
+++ src/bin/crawler.rs
@@ -1,4 +1,5 @@
 use clap::{arg, command};
+use scraper::{Html, Selector};
 use spider::configuration::Configuration;
 use spider::tokio;
 use spider::website::Website;
@@ -7,17 +8,18 @@ use spider::website::Website;
 async fn main() {
     let matches = command!()
         .arg(arg!(-u --url <URL> "Sets a start url").required(true))
+        // .arg(arg!(-w --word <WORD> "Sets word").required(true))
         .get_matches();
 
     let url = matches.get_one::<String>("url").unwrap();
+    // let word = matches.get_one::<String>("word").unwrap().clone();
 
     let config = Configuration::new()
+        .with_redirect_limit(111)
+        .with_caching(true)
+        // .with_respect_robots_txt(true)
         // .with_user_agent(Some("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"))
         // .with_chrome_intercept(cfg!(feature = "chrome_intercept"), true)
-        // .with_depth(0)
-        // .with_redirect_limit(10000)
-        // .with_caching(true)
-        // .with_respect_robots_txt(true)
         .build();
 
     let mut website = Website::new(url)
@@ -29,10 +31,31 @@ async fn main() {
 
     tokio::spawn(async move {
         while let Ok(res) = rx.recv().await {
-            println!("{:?}", res.get_url());
+            let html = res.get_html();
+            let url = res.get_url();
+
+            let fragment = Html::parse_fragment(&html);
+
+            let selector = Selector::parse(r#"h1[data-qa="vacancy-title"]"#).unwrap();
+
+            for e in fragment.select(&selector) {
+                let vacancy_title = e.inner_html();
+
+                println!("{vacancy_title}");
+                println!("url: {url}");
+                println!();
+            }
         }
     });
 
     website.crawl_smart().await;
     website.unsubscribe();
+}
+
+fn word_count(s: &str, word: &str) -> usize {
+    s.to_lowercase().matches(word).count()
+}
+
+fn word_is_present(s: &str, word: &str) -> bool {
+    word_count(s, word) > 0
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use reqwest::header;
 use reqwest::Client;
 use scraper::{Html, Selector};
 use std::collections::HashMap;
+use std::fs;
 
 #[tokio::main]
 async fn main() {
@@ -9,30 +10,21 @@ async fn main() {
 
     headers.insert("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36".parse().unwrap());
 
-    let params = {
-        let mut params = HashMap::new();
-        params.insert("v", "KmzvSo90Zj4".to_string());
-        params
-    };
-
     let client = Client::new();
     let response = client
-        .get("https://www.youtube.com/watch")
+        .get("https://hh.ru/vacancy/101946855?hhtmFrom=vacancy_search_list")
         .headers(headers)
-        .query(&params)
         .send()
         .await
         .unwrap();
 
     assert!(response.status().is_success());
 
-    // println!("{}", response.text().await.unwrap());
+    let fragment = Html::parse_fragment(&response.text().await.unwrap());
 
-    let html = Html::parse_fragment(&response.text().await.unwrap());
-
-    let selector = Selector::parse("title").unwrap();
-
-    let h1 = html.select(&selector).next().unwrap();
-
-    println!("{}", h1.html());
+    let selector = Selector::parse(r#"h1[data-qa="vacancy-title"]"#).unwrap();
+    println!(
+        "{}",
+        fragment.select(&selector).next().unwrap().inner_html()
+    );
 }
